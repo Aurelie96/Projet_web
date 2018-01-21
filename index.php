8ff6e28a9f955ca2db01743329eb5e2109c029f2
vergerk <vergerkhevin@vergerk.fr>
2018-01-21 15:19:41 +0100

    Début de test pour affichage avec Silex

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$app = new Silex\Application();
+
+/*
 require_once('./src/DAO/DAO.php');
 require_once('./src/Domain/Eleve.php');
 require_once('./src/DAO/EleveDAO.php');
-
+*/
 echo ('hello');
 $eleve = EleveDAO()->connectEleve('test', 'test');
