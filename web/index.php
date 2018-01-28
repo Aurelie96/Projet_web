185f86b96f1571d67bfcac6e9078b68ae090aaf9
vergerk <vergerkhevin@vergerk.fr>
2018-01-28 15:16:42 +0100

    début modification / correspondance GSB

--- web/index.php
+++ web/index.php
@@ -1,9 +1,15 @@
 <?php
 
+date_default_timezone_set('Europe/Paris');
 require_once(__DIR__ . '/../vendor/autoload.php');
+
 $app = new Silex\Application();
 
-require_once( __DIR__.'/../app/routes.php');
+require_once(__DIR__.'/../app/config/dev.php');
+require_once(__DIR__.'/../app/app.php');
+require_once(__DIR__.'/../app/routes.php');
 
 $app->run();
 echo ('hello');
+
+?>
