733940080016f008d826e82308366d2e9a38bb90
vergerk <vergerkhevin@vergerk.fr>
2018-01-28 15:37:18 +0100

    Suite modifications

--- app/routes.php
+++ app/routes.php
@@ -4,13 +4,15 @@
 $app->get('/', function() {
     echo('test fonction page d\'accueil');
 
-    require('./../src/DAO/DAO.php');
+    /*require('./../src/DAO/DAO.php');
     require('./../src/Domain/Eleve.php');
-    require('./../src/DAO/EleveDAO.php');
+    require('./../src/DAO/EleveDAO.php');*/
 
     ob_start();
-    $eleve = EleveDAO()->connectEleve('test', 'test');
-    var_dump($eleve);
+    echo "test";
+    /*$eleve = EleveDAO()->connectEleve('test', 'test');*/
+    $view = ob_get_clean();
+    var_dump($view);
 });
 
 ?>
